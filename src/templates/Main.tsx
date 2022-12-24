257d3080679d25b22680fbe6712ce3b4180d40f4
Fitra R <fitra.rahman@usedeall.com>
2022-12-24 22:10:22 +0700

    refactor(*): remove heroicon & update layout

--- src/templates/Main.tsx
+++ src/templates/Main.tsx
@@ -6,9 +6,9 @@ type TProps = {
 
 const Main: FC<TProps> = ({ children }) => {
   return (
-    <div className="grid h-screen grid-cols-[300px,1fr]">
+    <div className="grid h-screen lg:grid-cols-[300px,1fr]">
       {/* sidebar */}
-      <div className="bg-base-200"></div>
+      <div className="hidden bg-base-200 lg:block"></div>
       {/* content */}
       <div className="h-full w-full overflow-y-auto px-9 py-10">{children}</div>
     </div>
